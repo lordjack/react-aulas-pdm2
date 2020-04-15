ce5d06d4cd7a18168455de821da16de2fc2edebf
lordjack <lordjackson@gmail.com>
2020-04-15 10:45:27 -0300

    1 binaries uploaded

--- ExCss.js
+++ ExCss.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './folha.module.css';
-import imgEx from './img/imgEx.png';
+import imgEx2 from 'imgEx2.jpg';
 
 class ExCss extends React.Component {
   render() {
@@ -11,11 +11,7 @@ class ExCss extends React.Component {
       fontFamily: "Arial",
       fontSize: 18
     };
-    const imgEx = {
-         width: 46,
-         height: 44,
-       
-    };
+
     const myBord = {
        borderStyle: 'solid'
     };
@@ -25,7 +21,8 @@ class ExCss extends React.Component {
       <p style={{color:'red'}} > Add seu estilo</p>
 
        <h3 style={myBord} > Chamada Folha de Estilo </h3>
-       <img src={imgEx} alt="logo" />
+       <img src='imgEx2.jpg' alt="logo" />
+       <img src="paris.jpg" alt="Paris" style="width:150px" />
       </div>
     );
   }
